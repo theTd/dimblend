377dd93c8c609171313f3897de56eeac3ab11f1b
theTd <cui@starrel.org>
2026-09-05 03:08:08 +0800

    Narrow rotating bands from 4096 to 2048.

--- src/main/java/dimblend/worldgen/BandIndex.java
+++ src/main/java/dimblend/worldgen/BandIndex.java
@@ -3,7 +3,7 @@ package dimblend.worldgen;
 import net.minecraft.world.level.ChunkPos;
 
 public final class BandIndex {
-    public static final int DEFAULT_BAND_SIZE = 4096;
+    public static final int DEFAULT_BAND_SIZE = 2048;
     public static final int SEAM_WIDTH = 32;
     public static final int OVERWORLD_BAND = 0;
 
